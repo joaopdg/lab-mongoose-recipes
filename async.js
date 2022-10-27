196c1f0d0327384366bf9f7d9f0136693ff22733
joaopdg <jpgon@live.com.pt>
2022-10-27 12:33:44 +0100

    lab done with async/await instead of then/catch

---
+++ async.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
+
+const data = require("./data");
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app-async";
+
+const firstRecipe = data[0];
+
+const createData = async () => {
+  try {
+    // CONNECT TO DATA BASE
+    const connectingDB = await mongoose.connect(MONGODB_URI);
+    console.log(`Connected to the database: "${connectingDB.connection.name}"`);
+
+    // ADD ONE RECIPE
+    const oneRecipe = await Recipe.create(firstRecipe);
+    console.log(`New recipe added: ${oneRecipe.title}`);
+
+    // ADD MANY RECIPES
+    const manyRecipes = await Recipe.insertMany(data);
+    manyRecipes.forEach((recipe) =>
+      console.log(`New recipe added: ${recipe.title}`)
+    );
+
+    // UPDATE ONE RECIPE
+    const updateRecipe = await Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+    console.log(`Recipe Updated: ${updateRecipe}`);
+
+    // DELETE RECIPE
+    const deleteRecipe = await Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log(`Recipe Deleted`);
+
+    // DISCONNECT FROM DATA BASE
+    const disconnectDB = await mongoose.disconnect();
+    console.log("Disconnected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+createData();
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 const firstRecipe = data[0];
 
+// CONNECT TO DATA BASE
 mongoose
   .connect(MONGODB_URI)
   .then((x) => {
@@ -14,20 +15,35 @@ mongoose
   })
 
   .then(() => {
-    /*     Recipe.create(firstRecipe)
-    .then((newRecipe) => console.log(`New recipe added: ${newRecipe.title}`))
-    .catch((err) => console.log(err)); */
-    /*     Recipe.insertMany(data)
-    .then((recipes) => recipes.forEach((recipe) => console.log(`New Recipe added: ${recipe.title}`)))
-    .catch((err) => console.log(err)) */
-    /*     Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese' }, {duration: 100})
+    // ADD ONE RECIPE
+    Recipe.create(firstRecipe)
+      .then((newRecipe) => console.log(`New recipe added: ${newRecipe.title}`))
+      .catch((err) => console.log(err));
+
+    // ADD MANY RECIPES
+    Recipe.insertMany(data)
+      .then((recipes) =>
+        recipes.forEach((recipe) =>
+          console.log(`New Recipe added: ${recipe.title}`)
+        )
+      )
+      .catch((err) => console.log(err));
+
+    // UPDATE ONE RECIPE
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    )
       .then((recipe) => console.log(`Recipe Updated: ${recipe}`))
-      .catch((err) => console.log(err)); */
-    /*     Recipe.deleteOne({ title: "Carrot Cake" })
+      .catch((err) => console.log(err));
+
+    // DELETE RECIPE
+    Recipe.deleteOne({ title: "Carrot Cake" })
       .then((recipe) => console.log(`Recipe Deleted`))
-      .catch((err) => console.log(err)); */
+      .catch((err) => console.log(err));
   })
 
+  // DISCONNECT FROM DATA BASE
   .then(() => mongoose.disconnect(() => console.log("Disconnected")))
 
   .catch((error) => {
